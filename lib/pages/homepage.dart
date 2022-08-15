7b97a9edc8ca29da2ccec1609c113315d245ded4
DaeYoung <cosmosghost@gmail.com>
2022-08-15 21:11:10 +0900

    Flutter Toast 샘플 프로젝트

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Toast Sample'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('btn'),
+            ),
+            const Text('hi'),
+          ],
+        ),
+      ),
+    );
+  }
+}
